d39b51b205012ce3649b774ca5d6ea830dc20898
Kulbir Saini <saini@saini.co.in>
2013-06-12 02:41:16 +0530

    change default values for cache swap high/low

--- videocache/vcoptions.py
+++ videocache/vcoptions.py
@@ -122,7 +122,7 @@ class VideocacheOptions:
             # Options not in configuration file
             klass.queue_dump_file = mainconf.queue_dump_file
             klass.version = '2.3'
-            klass.revision = 'g1306a29'
+            klass.revision = 'f4974d639'
             # General Options
             klass.enable_videocache = int(mainconf.enable_videocache)
             klass.offline_mode = int(mainconf.offline_mode)
@@ -177,8 +177,8 @@ class VideocacheOptions:
             else:
                 klass.cleanup_order = 'access_count ASC, access_time ASC, size DESC'
             klass.filelist_rebuild_interval = int(mainconf.filelist_rebuild_interval)
-            cache_swap_low = min(max(int(mainconf.cache_swap_low), 85), 98)
-            cache_swap_high = min(max(int(mainconf.cache_swap_high), 90), 99)
+            cache_swap_low = min(max(int(mainconf.cache_swap_low), 30), 90)
+            cache_swap_high = min(max(int(mainconf.cache_swap_high), 40), 93)
             if cache_swap_low > cache_swap_high:
                 cache_swap_low, cache_swap_high = cache_swap_high, cache_swap_low
             elif cache_swap_low == cache_swap_high:
