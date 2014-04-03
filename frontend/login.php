c9461d5b619d33c2cc34f70d4fb9102c6c95e7bd
Kulbir Saini <saini@saini.co.in>
2014-04-03 22:12:45 +0000

    Vertically centered nice login page

--- frontend/login.php
+++ frontend/login.php
@@ -1,30 +1,44 @@
 <?php $current_page = 'login' ?>
-<?php include('./shared/prefix-html.php') ?>
-<div id="content" class="text-center full">
-  <div class="container-fluid">
-    <h1>VideoCache Web Frontend</h1>
-    <p>Manage your VideoCache server via a responsive web interface.</p>
-    <div class="row login-box">
-      <div class="col-xs-8 col-sm-4 col-md-2 well well-lg centered">
-        <form action="<?php print ROOT_PATH ?>/login" method="POST" role="form" class="form-horizontal">
-          <div class="form-group">
-            <div class="col-sm-12 col-xs-12 col-md-12">
-              <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+<?php include('./shared/functions.php'); ?>
+<?php include('./shared/require-login.php'); ?>
+<!Doctype html>
+<html lang="en" class="stretch-height">
+  <?php include('./shared/head.php') ?>
+  <body class="stretch-height no-margin no-padding">
+    <style type="text/css">
+      #content { margin: 0; padding: 0; }
+      .wrapper { width: 100%; }
+    </style>
+    <div id="content" class="text-center stretch-height">
+      <div class="container-fluid stretch-height vcenter">
+        <div class="wrapper">
+          <div class="row">
+            <h1>VideoCache Web Frontend</h1>
+            <p>Manage your VideoCache server via a responsive web interface.</p>
+          </div>
+          <div class="row login-box">
+            <div class="col-xs-8 col-sm-4 col-md-2 well well-lg centered">
+              <form action="<?php print ROOT_PATH ?>/login" method="POST" role="form" class="form-horizontal">
+                <div class="form-group">
+                  <div class="col-sm-12 col-xs-12 col-md-12">
+                    <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="col-sm-12 col-xs-12 col-md-12">
+                    <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+                  </div>
+                </div>
+                <div class="form-group text-left">
+                  <div class="col-sm-12 col-xs-12 col-md-12">
+                    <button type="submit" id="login" class="btn btn-default">Login</button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
-          <div class="form-group">
-            <div class="col-sm-12 col-xs-12 col-md-12">
-              <input type="password" class="form-control" name="password" id="password" placeholder="Password">
-            </div>
-          </div>
-          <div class="form-group text-left">
-            <div class="col-sm-12 col-xs-12 col-md-12">
-              <button type="submit" id="login" class="btn btn-default">Login</button>
-            </div>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-<?php include('./shared/suffix-html.php') ?>
+  </body>
+</html>
